1e86eabef150ff9a72871ffc041ebfcffe74611b
Austin Asbury <acasbury25@aol.com>
2019-10-26 18:05:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy_hash = {:name = params[:name], :age = params[:months_old], :breed = params[:breed]}
-    @puppy = Puppy.new(@puppy_hash)
+    puppy_hash = {:name = params[:name], :age = params[:months_old], :breed = params[:breed]}
+    @puppy = Puppy.new(puppy_hash)
     erb :display_puppy
   end
 end
